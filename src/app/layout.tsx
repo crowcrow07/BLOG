804906c700df26f2ab09a754700de95efdd866da
crowcrow07 <skathd3402@gmail.com>
2024-01-15 23:40:10 +0900

    feat : layout bg img 설정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Noto_Sans_KR } from "next/font/google";
-import "./globals.css";
+import "../styles/globals.css";
+import Image from "next/image";
 
 const notoSans = Noto_Sans_KR({
   subsets: ["latin"],
@@ -19,7 +20,24 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={notoSans.className}>{children}</body>
+      <body className={`relative ${notoSans.className}`}>
+        <div className="fixed w-[80vw] h-screen right-0">
+          <Image
+            src="/images/background.jpeg"
+            alt="BackgroundImg"
+            fill
+            style={{
+              objectFit: "cover",
+              objectPosition: "center center",
+            }}
+          />
+        </div>
+        <div className="fixed flex z-50 text-white bg-black bottom-0 right-0">
+          <div>아이콘 1</div>
+          <div>아이콘 2</div>
+        </div>
+        {children}
+      </body>
     </html>
   );
 }
