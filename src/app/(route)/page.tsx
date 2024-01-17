4c2fe3c544106ebfbde78506039a5adb8e4d6bdb
crowcrow07 <skathd3402@gmail.com>
2024-01-17 22:51:34 +0900

    fixed : 모바일 가로모드 반응형 고려

--- src/app/(route)/page.tsx
+++ src/app/(route)/page.tsx
@@ -1,17 +1,12 @@
 import Header from "@/components/aboutme/header";
+import Main from "@/components/aboutme/main";
 
 export default function Home() {
   return (
-    <>
+    <div className="overflow-y-scroll">
       <Header />
-      <main className="bg-white relative">
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
-      </main>
+      <Main />
       <footer></footer>
-    </>
+    </div>
   );
 }
