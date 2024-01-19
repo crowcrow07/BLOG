ebe1d79043d679e80af6d2832d43c12db1d72e6f
crowcrow07 <skathd3402@gmail.com>
2024-01-19 11:12:21 +0900

    feat : Card 컴포넌트 퍼블리싱

---
+++ src/app/components/aboutme/main/Card.tsx
@@ -0,0 +1,39 @@
+import { ReactNode } from "react";
+import Image from "next/image";
+
+import styles from "@/styles/Main.module.scss";
+
+type Props = {
+  children: ReactNode;
+};
+
+// export default function Card({ children, ...props }: Props) {
+export default function Card() {
+  // 받아야할 데이터 : id, image url, title, subTitle, contents, tag, date
+  return (
+    <div className="h-full flex flex-col">
+      <div className={`${styles.none} ${styles.imageContainer}`}>
+        <Image
+          src={"/images/background.jpeg"}
+          alt="thumnail"
+          className="object-contain"
+          fill
+        />
+      </div>
+      <div className="flex-[2] p-4  flex flex-col justify-between ">
+        <div>
+          <div className="text-xl font-bold w-full text-start">
+            제목이 너무 길면 어떻게 보이게 되는거죠?
+          </div>
+          <div className="w-full text-start">부제목</div>
+        </div>
+        <div className={`${styles.none} text-sm w-full text-start`}>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni, atque?
+        </div>
+      </div>
+      <div className="flex-[1] px-4 w-full text-start text-xs flex h-full items-center">
+        태그, 날짜
+      </div>
+    </div>
+  );
+}
--- src/app/components/aboutme/main/index.tsx
+++ src/app/components/aboutme/main/index.tsx
@@ -1,21 +1,29 @@
+import Image from "next/image";
+
+import Card from "./Card";
+
 import styles from "@/styles/Main.module.scss";
 
 export default function Main() {
   return (
     <main className={`${styles.mainContainer}`}>
       <div className={`${styles.contentsContainer}`}>
-        <h2 className="text-5xl mb-6 font-bold">소제목이 들어갈듯</h2>
+        <h2 className="my-6 pb-4 font-bold">개발 컨텐츠</h2>
         <div className={`${styles.gridContainer}`}>
-          <div>이력서</div>
+          <Card />
           <div>Project</div>
           <div>Share</div>
           <div>학력 or 교육</div>
           <div>스터디</div>
           <div>지금까지 블로그 포스팅한것들</div>
         </div>
-        <h2 className="text-5xl mt-6 font-bold">
-          개발이랑 관련 없는 소제목 들어갈듯
-        </h2>
+        <h2 className="mt-12 mb-8 pb-4 font-bold">일상 컨텐츠</h2>
+        <div className={`${styles.gridContainer}`}>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+          <div>1</div>
+        </div>
       </div>
     </main>
   );
