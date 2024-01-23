53f3cdea4ec7b5aff057c783fa724742561d8314
crowcrow07 <skathd3402@gmail.com>
2024-01-23 22:50:10 +0900

    refactor : 환경변수 설정

--- src/app/api/route.ts
+++ src/app/api/route.ts
@@ -1,42 +1,43 @@
 import mysql, { ConnectionOptions } from "mysql2/promise";
+import { NextRequest, NextResponse } from "next/server";
 
 const access: ConnectionOptions = {
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "1234",
-  database: "blog",
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
 };
 
 export const dynamic = "force-dynamic"; // defaults to auto
-export async function GET(request: Request) {
+export async function GET(request: NextRequest) {
   try {
     const connection = await mysql.createConnection(access);
 
-    let sql = "SELECT * FROM blog";
+    let sql = "SELECT * FROM CATEGORY_TB";
 
     // 쿼리 실행
-    const [rows, fields] = await connection.execute(sql);
+    const [result, fields] = await connection.query(sql);
 
     connection.end();
-    console.log("rows 123", rows);
+
     // HTTP 응답을 반환
-    return Response.json({ data: rows });
+    return NextResponse.json({ data: result });
   } catch (err) {
     console.log(err);
 
     // 에러 상황에서도 적절한 응답을 반환
-    return new Response("Internal Server Error", {
+    return new NextResponse("Internal Server Error", {
       status: 500,
       headers: { "Content-Type": "text/plain" },
     });
   }
 }
-export async function POST(request: Request) {
+export async function POST(request: NextRequest) {
   // mysql connection create or edit
   // fs.write()
 }
-export async function DELETE(request: Request) {
+export async function DELETE(request: NextRequest) {
   // mysql connection delete
   // fs.delete()
 }
