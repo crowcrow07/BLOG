ddd9c97f91a0c2920c94c5b16614ce9bb4b7a6b5
crowcrow07 <skathd3402@gmail.com>
2024-02-01 14:24:08 +0900

    feat : export email logo

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -1,5 +1,6 @@
 import IconArrow from "./arrow-up-sharp.svg";
 import IconDarkMode from "./dark-mode.svg";
 import IconLightMode from "./sun-light.svg";
+import IconEmail from "./email.svg";
 
-export { IconArrow, IconDarkMode, IconLightMode };
+export { IconArrow, IconDarkMode, IconLightMode, IconEmail };
