d9a61fca36d56477f290038d7d8a8d5953a2be99
crowcrow07 <skathd3402@gmail.com>
2024-02-03 15:12:48 +0900

    feat : logout svg export

--- public/svgs/index.ts
+++ public/svgs/index.ts
@@ -3,5 +3,13 @@ import IconDarkMode from "./dark-mode.svg";
 import IconLightMode from "./sun-light.svg";
 import IconEmail from "./email.svg";
 import IconLogin from "./login.svg";
+import IconLogout from "./logout.svg";
 
-export { IconArrow, IconDarkMode, IconLightMode, IconEmail, IconLogin };
+export {
+  IconArrow,
+  IconDarkMode,
+  IconLightMode,
+  IconEmail,
+  IconLogin,
+  IconLogout,
+};
