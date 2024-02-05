0d7a967f5e85e887fdd803ecbf59a8f2f429c18d
crowcrow07 <skathd3402@gmail.com>
2024-02-05 14:28:55 +0900

    fixed : 버튼 리스트 틀 고정

--- src/app/components/aboutme/header/SignInButton.tsx
+++ src/app/components/aboutme/header/SignInButton.tsx
@@ -1,19 +1,24 @@
 "use client";
-
+import React from "react";
 import { signIn, signOut, useSession } from "next-auth/react";
 import { IconLogin, IconLogout } from "@/public/svgs";
-import React from "react";
+import Image from "next/image";
 
 function SignInButton() {
-  const { data: session } = useSession();
+  const { data: session, status } = useSession();
+
+  if (status === "loading") {
+    return (
+      <div className="animate-pulse rounded-full bg-gray-500 opacity-90 w-full h-full"></div>
+    );
+  }
 
   if (session && session.user) {
     return (
-      <div>
-        <button onClick={() => signOut()}>
-          <IconLogout width={"4rem"} height={"4rem"} />
-        </button>
-      </div>
+      <button className="w-full h-full" onClick={() => signOut()}>
+        {/* <IconLogout width={"4rem"} height={"4rem"} /> */}
+        <Image src={session.user.picture} alt="profile" fill loading="lazy" />
+      </button>
     );
   }
   return (
