3554b5b1fea544e4ffc633248a473c18e7c92994
crowcrow07 <skathd3402@gmail.com>
2024-02-14 18:55:39 +0900

    feat : 카카오 로그인, 토큰 role 추가

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,58 +1,40 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
-import CredentialsProvider from "next-auth/providers/credentials";
+import KakaoProvider from "next-auth/providers/kakao";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID || "",
       clientSecret: process.env.GOOGLE_CLIENT_SECRET || "",
+
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+        };
+      },
     }),
-    CredentialsProvider({
-      // The name to display on the sign in form (e.g. "Sign in with...")
-      name: "Credentials",
-      // `credentials` is used to generate a form on the sign in page.
-      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
-      // e.g. domain, username, password, 2FA token, etc.
-      // You can pass any HTML attribute to the <input> tag through the object.
-      credentials: {
-        username: {
-          label: "이메일",
-          type: "text",
-          placeholder: "이메일 주소 입력 요망",
-        },
-        password: { label: "비밀번호", type: "password" },
-      },
-      async authorize(credentials, req) {
-        // Add logic here to look up the user from the credentials supplied
-        const res = await fetch(`${process.env.NEXTAUTH_URL}/api/login`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-          },
-          body: JSON.stringify({
-            username: credentials?.username,
-            password: credentials?.password,
-          }),
-        });
-        const user = await res.json();
-        console.log("user", user);
-
-        if (user && !user.error) {
-          // Any object returned will be saved in `user` property of the JWT
-          return user;
-        } else {
-          // If you return null then an error will be displayed advising the user to check their details.
-          return null;
-
-          // You can also Reject this callback with an Error thus the user will be sent to the error page with the error message as a query parameter
-        }
-      },
+    KakaoProvider({
+      clientId: process.env.KAKAO_CLIENT_ID || "",
+      clientSecret: process.env.KAKAO_CLIENT_SECRET || "",
     }),
   ],
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60, // 30 days
+  },
 
   callbacks: {
     async jwt({ token, user }) {
+      const isAdminUser = isAdmin(token.email!, token.name!);
+      if (isAdminUser) {
+        token.role = "admin";
+      } else {
+        token.role = "user";
+      }
       return { ...token, ...user };
     },
 
@@ -63,7 +45,16 @@ const handler = NextAuth({
   },
   pages: {
     signIn: "/signin",
+    error: "/",
   },
 });
 
 export { handler as GET, handler as POST };
+
+const isAdmin = (email: string, name: string) => {
+  if (name !== "김민재") return false;
+  if (email === "skathd3402@gmail.com" || email === "skathd3402@naver.com") {
+    return true;
+  }
+  return false;
+};
