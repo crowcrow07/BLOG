7526f27a361697c4e45926b449859b71af59d50b
crowcrow07 <skathd3402@gmail.com>
2024-02-14 18:55:15 +0900

    feat : 관리자페이지 관리자만 접근 가능

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,5 +1,14 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+export default withAuth((req) => {
+  if (req.nextUrl.pathname.startsWith("/admin")) {
+    if (req.nextauth.token!.role !== "admin") {
+      return NextResponse.redirect(new URL("/", req.url));
+    }
+  }
+});
 
 export const config = {
-  matcher: ["/userposts/:path*"],
+  matcher: ["/userposts/:path*", "/admin/:path*"],
 };
