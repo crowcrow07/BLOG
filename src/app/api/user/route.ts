73317ec04b521905f8650cb7cbe94e28b7c677f7
crowcrow07 <skathd3402@gmail.com>
2024-02-14 19:03:05 +0900

    fixed : 안쓰는 코드 일단 주석

--- src/app/api/user/route.ts
+++ src/app/api/user/route.ts
@@ -1,41 +1,41 @@
-import mysql, { ConnectionOptions } from "mysql2/promise";
-import { NextRequest, NextResponse } from "next/server";
-import * as bcrypt from "bcrypt";
+// import mysql, { ConnectionOptions } from "mysql2/promise";
+// import { NextRequest, NextResponse } from "next/server";
+// import * as bcrypt from "bcrypt";
 
-const access: ConnectionOptions = {
-  host: process.env.DB_HOST,
-  port: Number(process.env.DB_PORT),
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  database: process.env.DB_NAME,
-};
+// const access: ConnectionOptions = {
+//   host: process.env.DB_HOST,
+//   port: Number(process.env.DB_PORT),
+//   user: process.env.DB_USER,
+//   password: process.env.DB_PASSWORD,
+//   database: process.env.DB_NAME,
+// };
 
-interface RequestBody {
-  name: string;
-  email: string;
-  password: string;
-}
+// interface RequestBody {
+//   name: string;
+//   email: string;
+//   password: string;
+// }
 
-export async function POST(request: NextRequest, response: NextResponse) {
-  try {
-    // db에 전송하는 코드
-    const body: RequestBody = await request.json();
-    const connection = await mysql.createConnection(access);
-    const hashPassword = await bcrypt.hash(body.password, 10);
+// export async function POST(request: NextRequest, response: NextResponse) {
+//   try {
+//     // db에 전송하는 코드
+//     const body: RequestBody = await request.json();
+//     const connection = await mysql.createConnection(access);
+//     const hashPassword = await bcrypt.hash(body.password, 10);
 
-    const sql = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`;
-    const values = [body.name, body.email, hashPassword];
+//     const sql = `INSERT INTO user (name, email, password) VALUES (?, ?, ?)`;
+//     const values = [body.name, body.email, hashPassword];
 
-    // 쿼리 실행
-    await connection.query(sql, values);
+//     // 쿼리 실행
+//     await connection.query(sql, values);
 
-    connection.end();
+//     connection.end();
 
-    const { password, ...result } = body;
+//     const { password, ...result } = body;
 
-    return NextResponse.json({ success: true, data: result });
-  } catch (error) {
-    console.error("Error uploading image:", error);
-    NextResponse.json({ message: "Error uploading image" });
-  }
-}
+//     return NextResponse.json({ success: true, data: result });
+//   } catch (error) {
+//     console.error("Error uploading image:", error);
+//     NextResponse.json({ message: "Error uploading image" });
+//   }
+// }
