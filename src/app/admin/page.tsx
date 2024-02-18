b701b128861d5c798c37edd285ea998026985727
crowcrow07 <skathd3402@gmail.com>
2024-02-18 15:18:39 +0900

    feat : 어드민 페이지

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,10 +1,49 @@
+"use client";
+
+import { useState } from "react";
+
 import AdminRoute from "../utils/AdminRoute";
+import CreatePost from "@/components/admin/CreatePost";
+import ManagePost from "@/components/admin/ManagePost";
+import ManageCategory from "@/components/admin/ManageCategory";
+import ManageTag from "@/components/admin/ManageTag";
+
+import styles from "@/styles/Admin.module.scss";
 
 export default function Admin() {
+  const [page, setPage] = useState("createPost");
+
+  const navHandler = (type: string) => {
+    setPage(type);
+  };
+
+  const viewPage = (type: string) => {
+    switch (type) {
+      case "createPost":
+        return <CreatePost />;
+      case "managePost":
+        return <ManagePost />;
+      case "manageCategory":
+        return <ManageCategory />;
+      case "manageTag":
+        return <ManageTag />;
+      default:
+        return <div>404</div>;
+    }
+  };
+
   return (
-    <div>
+    <header className={styles.headerContainer}>
       <AdminRoute />
-      어드민 페이지
-    </div>
+      <nav className={styles.navContainer}>
+        <button onClick={() => navHandler("createPost")}>글 작성</button>
+        <button onClick={() => navHandler("managePost")}>글 관리</button>
+        <button onClick={() => navHandler("manageCategory")}>
+          카테고리 관리
+        </button>
+        <button onClick={() => navHandler("manageTag")}>태그 관리</button>
+      </nav>
+      <section className={styles.introContainer}>{viewPage(page)}</section>
+    </header>
   );
 }
