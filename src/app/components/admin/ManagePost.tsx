14712775a29f87b20d30cdaa37882bac99a6d62e
crowcrow07 <skathd3402@gmail.com>
2024-02-25 15:28:21 +0900

    feat : 글관리 컴포넌트

--- src/app/components/admin/ManagePost.tsx
+++ src/app/components/admin/ManagePost.tsx
@@ -1,3 +1,36 @@
+import { useState, useEffect } from "react";
+import Card from "./Card";
+
+interface getData {
+  id: number;
+  title: string;
+  subTitle: string;
+  categoryId: number;
+  content: string;
+  thumbnail: string;
+  //   tags: TagItem[];
+}
+
 export default function ManagePost() {
-  return <div>글 관리</div>;
+  useEffect(() => {
+    const getPost = async () => {
+      const result = await fetch("http://localhost:3000/api/post");
+      const data = await result.json();
+      console.log(data);
+      setList(data.data);
+    };
+
+    getPost();
+  }, []);
+
+  const [list, setList] = useState<getData[]>([]);
+
+  return (
+    <div className="p-4">
+      {list &&
+        list.map((post: getData) => {
+          return <Card key={post.id} post={post} />;
+        })}
+    </div>
+  );
 }
