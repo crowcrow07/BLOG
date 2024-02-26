ba9d5414ecb1b756a7bd92a48f8af5da8436659a
crowcrow07 <skathd3402@gmail.com>
2024-02-26 16:41:14 +0900

    fixed : 타입 수정

--- src/app/components/aboutme/main/Card.tsx
+++ src/app/components/aboutme/main/Card.tsx
@@ -5,12 +5,13 @@ import styles from "@/styles/Main.module.scss";
 
 interface Data {
   id: number;
-  image_url: string;
+  categoryId: number;
   title: string;
   subtitle: string;
-  contents: string;
-  tag: string;
-  date: string;
+  content: string;
+  createdAt: string;
+  modifiedAt: string;
+  thumbnail: string;
 }
 
 interface CardProps {
@@ -20,13 +21,22 @@ interface CardProps {
 // export default function Card({ children, ...props }: Props) {
 export default function Card({ data }: CardProps) {
   // 받아야할 데이터 : id, image url, title, subTitle, contents, tag, date, 이동할 경로
-  const { id, image_url, title, subtitle, contents, tag, date } = data;
+  const {
+    id,
+    categoryId,
+    title,
+    subtitle,
+    content,
+    createdAt,
+    modifiedAt,
+    thumbnail,
+  } = data;
 
   return (
     <div className="h-full flex flex-col">
       <div className={`${styles.none} ${styles.imageContainer}`}>
         <Image
-          src={`https://crow07.s3.ap-northeast-2.amazonaws.com/${image_url}`}
+          src={`https://crow07.s3.ap-northeast-2.amazonaws.com/${thumbnail}`}
           alt="thumnail"
           className="object-contain"
           fill
@@ -38,11 +48,11 @@ export default function Card({ data }: CardProps) {
           <div className="w-full text-start">{subtitle}</div>
         </div>
         <div className={`${styles.none} text-sm w-full text-start`}>
-          {contents}
+          {content}
         </div>
       </div>
       <div className="flex-[1] px-4 w-full text-start text-xs flex h-full items-center">
-        tag :{tag} date : {date}
+        data : {createdAt}
       </div>
     </div>
   );
